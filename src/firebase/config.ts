03f045aa4055bd434d2017e9a86e02fae8b71aa8
John Carlo Navarro <njcarlo@gmail.com>
2026-02-01 03:25:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3072837227-9a1db",
+  "appId": "1:230541701843:web:093bed12d24183523d5a26",
+  "apiKey": "AIzaSyBB6Un27ORb4jUCNKpHgZgiBmImtoyKsRE",
+  "authDomain": "studio-3072837227-9a1db.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "230541701843"
+};
